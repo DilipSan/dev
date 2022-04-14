8417628921a2ae13e63396377982da9408945ec4
DilipSan <sankaran.dilip@gmail.com>
2022-04-14 09:26:55 +0200

    Modif2

--- project-jee-2/src/main/java/fr/formation/inti/controllers/IndexController.java
+++ project-jee-2/src/main/java/fr/formation/inti/controllers/IndexController.java
@@ -29,6 +29,7 @@ public class IndexController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		out.println("en marche");
 	}
 
 	/**
